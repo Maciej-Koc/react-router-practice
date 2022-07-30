b5a80d735e42069f303563e30017022ae368d3ea
Maciej-Koc <mkoc2@asu.edu>
2022-07-30 01:15:25 -0500

    Included history object using useHistory hook to go to main page after adding quote.

--- src/components/pages/NewQuote.js
+++ src/components/pages/NewQuote.js
@@ -1,8 +1,12 @@
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../quotes/QuoteForm";
 
 export default function NewQuote() {
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+    history.push("/quotes");
   };
 
   return (
